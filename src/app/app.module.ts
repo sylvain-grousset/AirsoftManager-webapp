74e7429f80d08db710fcd7b6f02cd19e80834112
IPGARDE\sgrousset <sylvain.grousset1@gmail.com>
2023-09-07 14:06:27 +0200

    Ajout d'imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogInscriptionComponent } from './dialog-inscription/dialog-inscription.component'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogInscriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
