f5e6a1b2e5b2d0d68339c3674eb294ccbec22b49
IPGARDE\sgrousset <sylvain.grousset1@gmail.com>
2023-09-22 11:08:02 +0200

    ajout fenêtre modale

--- src/app/dialog-inscription/dialog-inscription.component.ts
+++ src/app/dialog-inscription/dialog-inscription.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { InscriptionParticipant } from '../interface/InscriptionParticipant';
+import { ApiService } from '../service/api-service.service';
 
 @Component({
   selector: 'app-dialog-inscription',
@@ -8,7 +11,38 @@ import { Inject } from '@angular/core';
   styleUrls: ['./dialog-inscription.component.scss']
 })
 export class DialogInscriptionComponent {
-constructor(@Inject(MAT_DIALOG_DATA) public data:any){
-  console.log(data.sessionID);
+
+  selectedSessionID!: number;
+  public participantForm = new FormGroup({
+    participantNom: new FormControl('', [Validators.required]),
+    participantPrenom: new FormControl('', [Validators.required]),
+    participantEmail: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+  });
+
+constructor(@Inject(MAT_DIALOG_DATA) public data:any, private apiService: ApiService){
+  this.selectedSessionID = data.sessionID;
 }
+
+public onSubmit(e: any) {
+  if(!this.participantForm.invalid){
+    let leParticipant: InscriptionParticipant = {
+      sessionID: this.selectedSessionID,
+      nom: this.participantForm.controls.participantNom.value!,
+      prenom: this.participantForm.controls.participantPrenom.value!,
+      email: this.participantForm.controls.participantEmail.value!
+    }
+
+    this.apiService.inscriptionParticipant(leParticipant).subscribe(res => {
+      if(res == "Already registered"){
+        //L'utilisateur ne peut s'inscrire deux fois à la même session.
+      }else if(res == false){
+        //Erreur lors de l'insertion du participant à la session.
+      }else{
+        //Ok
+      }
+    });
+
+  }
+}
+
 }
