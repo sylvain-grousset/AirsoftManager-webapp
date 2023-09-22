edce8d9b0f66a938cf1f6077f867baeec5d98b74
IPGARDE\sgrousset <sylvain.grousset1@gmail.com>
2023-09-22 11:07:14 +0200

    Ajout inscriptionParticipant & modification getAllSessions

--- src/app/service/api-service.service.ts
+++ src/app/service/api-service.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Session } from '../interface/Session';
+import { InscriptionParticipant } from '../interface/InscriptionParticipant';
+import { SessionNbrParticipant } from '../interface/SessionNbrParticipant';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,21 @@ import { Session } from '../interface/Session';
 export class ApiService {
 
   API_URL = 'https://localhost:7289/api'
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };  
 
   constructor(private http: HttpClient) { }
 
   getAllSessions(){
-    return this.http.get<Session[]>(this.API_URL + '/Session/GetAll');
+    return this.http.get<SessionNbrParticipant[]>(this.API_URL + '/Session/GetAll');
+  }
+
+  inscriptionParticipant(leParticipant: InscriptionParticipant){
+    let data = JSON.stringify(leParticipant);
+    return this.http.post(this.API_URL + '/Participant/InscriptionParticipant', data, this.httpOptions); 
   }
 
 }
